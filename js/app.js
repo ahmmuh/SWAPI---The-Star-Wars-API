14598ed0a312dc0812d0ce8a047622243fe2f50d
ahmmuh <mukhtar1100@hotmail.com>
2021-03-20 22:18:06 +0100

    ok

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//created function with parameter query to search films from API
+//call showFilms with result as parameter
 const searchFilms = (query) => {
   const url = `https://swapi.dev/api/films/?search=${query}`;
 
@@ -5,34 +7,40 @@ const searchFilms = (query) => {
     .then((response) => response.json())
     .then((data) => {
       showFilms(data.results);
-    })
-    .catch((error) => {
-      const errElement = document.getElementsByClassName("errMsg"); //återkommer?
-      errElement.innerHTML = error;
-      console.log(errElement);
     });
 };
-let TimeOurToken = 0;
-
+//call onload event to wait for the whole page is loaded
+//get search input by it's ID, check if there an input, if it is -> keyup event to input
+//check if the input has 0 as value then return 0 otherwise call searchFilms() function and pass searchInput value
 window.onload = () => {
   const searchInput = document.querySelector("#search");
   if (searchInput) {
     searchInput.addEventListener("keyup", () => {
       if (searchInput.value.trim().length === 0) {
         return 0;
-      }
-      TimeOurToken = setTimeout(() => {
+      } else {
         searchFilms(searchInput.value);
-      }, 200);
+      }
     });
   }
 };
+// this function with films parameter, Here i use filter() method to check if the
+// new array containing the elements that match the set test
+// Returns an empty array if there are no matches
+//pass results from filter to displayFilms() function
 
 const showFilms = (films) => {
-  const filmResults = films.map((film) => film);
+  const filmResults = films.filter((film) => film);
   displayFilms(filmResults);
 };
+
+//displayFilms() function with results as parameter
+// select p with its class "lead" which has "Found movies appear here" by default in the html but set the length of the film result so you can se how many films was found.
+// loop trough films and created a few html element to show matched films on the html (index.html)
 const displayFilms = (results) => {
+  document.querySelector(
+    ".lead"
+  ).innerHTML = ` ${results.length} movie(s) Was found`;
   let cardBody = document.querySelector(".card-body");
   cardBody.innerHTML = "";
   results.forEach((film) => {
@@ -49,24 +57,15 @@ const displayFilms = (results) => {
       cardBody.appendChild(button);
       button.addEventListener("click", (e) => {
         e.preventDefault();
-        saveFilm(film);
+        saveFilm(film); // call saveFilm() function after clicking on the button (More info)
       });
-    } else {
-      const h4 = document.createElement("h4");
-      h4.innerHTML = "There is no a film";
-      cardBody.appendChild(h4);
-      alert("no");
     }
   });
 };
+
+// created saveFilm() function with film as parameter and the saved the film on the localStorage web API and opened film.html page to se more information about the selected film
 
 const saveFilm = (film) => {
   localStorage.setItem("key", JSON.stringify(film));
   location.href = "film.html";
 };
-
-setTimeout(() => {
-  window.location.reload = () => {
-    localStorage.clear();
-  };
-}, 5000);
--- js/film.js
+++ js/film.js
@@ -1,3 +1,4 @@
+//select back button with it ID, checked if the a button and then added click event and opened index.html
 const backBtn = document.querySelector("#backBtn");
 if (backBtn) {
   backBtn.addEventListener("click", () => {
@@ -5,6 +6,8 @@ if (backBtn) {
   });
 }
 
+// this function gets information from getSavedFilms() function which is below.
+// created html element and show the detail of the seleced film.
 const showSavedFilm = (savedFilm) => {
   const detailFilmTitle = document.querySelector("#detailFilmTitle");
   const director = document.querySelector(".director");
@@ -20,10 +23,12 @@ const showSavedFilm = (savedFilm) => {
   release_date.innerHTML = `Release: date: ${savedFilm.release_date}`;
   characters.innerHTML = `Characters: ${savedFilm.characters.length}`;
   species.innerHTML = `Species: ${savedFilm.species.length}`;
-  console.log(savedFilm);
 };
+// this function gets savedfilm from localStorage.getItem("key") web API and pass the data to showSavedFilm(savedFilm);
 const getSavedFilm = () => {
   let savedFilm = JSON.parse(window.localStorage.getItem("key"));
   showSavedFilm(savedFilm);
 };
+
+//call the function
 getSavedFilm();
